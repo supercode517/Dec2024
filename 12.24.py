e00135c46113bcf17d2e3382839d415e34f71900
richard lewis <richardlewisgood626@gmail.com>
2024-12-24 23:49:14 -0800

    Update 12.24.py

--- 12.24.py
+++ 12.24.py
@@ -2,18 +2,18 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.layers.merge import concatenate
+from tensorflow.keras.layers import concatenate
 from tensorflow.keras.utils import plot_model
 def inception_module(layer_in, f1, f2_in, f2_out, f3_in, f3_out, f4_out):
-conv1 = Conv2D(f1, (1,1), padding='same', activation='relu')(layer_in)
-conv3 = Conv2D(f2_in, (1,1), padding='same', activation='relu')(layer_in)
-conv3 = Conv2D(f2_out, (3,3), padding='same', activation='relu')(conv3)
-conv5 = Conv2D(f3_in, (1,1), padding='same', activation='relu')(layer_in)
-conv5 = Conv2D(f3_out, (5,5), padding='same', activation='relu')(conv5)
-pool = MaxPooling2D((3,3), strides=(1,1), padding='same')(layer_in)
-pool = Conv2D(f4_out, (1,1), padding='same', activation='relu')(pool)
-layer_out = concatenate([conv1, conv3, conv5, pool], axis=-1)
-return layer_out
+    conv1 = Conv2D(f1, (1,1), padding='same', activation='relu')(layer_in)
+    conv3 = Conv2D(f2_in, (1,1), padding='same', activation='relu')(layer_in)
+    conv3 = Conv2D(f2_out, (3,3), padding='same', activation='relu')(conv3)
+    conv5 = Conv2D(f3_in, (1,1), padding='same', activation='relu')(layer_in)
+    conv5 = Conv2D(f3_out, (5,5), padding='same', activation='relu')(conv5)
+    pool = MaxPooling2D((3,3), strides=(1,1), padding='same')(layer_in)
+    pool = Conv2D(f4_out, (1,1), padding='same', activation='relu')(pool)
+    layer_out = concatenate([conv1, conv3, conv5, pool], axis=-1)
+    return layer_out
 visible = Input(shape=(256, 256, 3))
 layer = inception_module(visible, 64, 96, 128, 16, 32, 32)
 layer = inception_module(layer, 128, 128, 192, 32, 96, 64)
